f5b7ad11fde65a2fda120d2fafa907a11ec2a778
Bianca <bsquintan@gmail.com>
2021-06-22 10:11:36 -0300

    changes search_field locale

--- back-end/app/controllers/profiles_controller.rb
+++ back-end/app/controllers/profiles_controller.rb
@@ -1,31 +1,31 @@
 class ProfilesController < ApplicationController
-    before_action :set_profile, only: %i[ show edit update destroy ]
+  before_action :set_profile, only: %i[ show edit update destroy ]
 
-    def index
-      if params[:search]
-        @profiles = User.search(params[:search])
-      else
-        @profiles = User.all
-      end
+  def index
+    if params[:search]
+      @profiles = User.search(params[:search])
+    else
+      @profiles = User.all
     end
-  
-    def show
+  end
+
+  def show
+    @profile = User.find(params[:id])
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_profile
+      params[:id] = current_user.id if current_user
       @profile = User.find(params[:id])
     end
 
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_profile
-        params[:id] = current_user.id if current_user
-        @profile = User.find(params[:id])
-      end
-  
-      # Only allow a list of trusted parameters through.
-      def profile_params
-        params.require(:profile).permit(:user_id)
-      end
+    # Only allow a list of trusted parameters through.
+    def profile_params
+      params.require(:profile).permit(:user_id)
+    end
 
-      def filtering_params(params)
-        params.slice(:username)
-      end
+    def filtering_params(params)
+      params.slice(:username)
+    end
 end
